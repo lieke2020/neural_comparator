b758b437067ea9c1f1a802ee880bd358e9e9eaeb
lieke2020 <71390417+lieke2020@users.noreply.github.com>
2021-06-22 18:28:44 +0200

    Add files via upload

--- neural_comparator_github.py
+++ neural_comparator_github.py
@@ -63,105 +63,83 @@ def cosine_similarity(inp1, inp2):
     cos_corr = np.dot(inp1, inp2)/vec_norm
     return cos_corr
 
-#%% Functions for matchnet input + load the CIFAR data_set
+#%% Define a class object for the Matchnet input data
 
-#Get dataset of 10000 normalized CIFAR-10 latent features
-def load_cifar_data():
-    with open("CIFAR_10_kaggle_feature_2.csv", 'r') as f:
-        csv_features = np.array(list(csv.reader(f, delimiter=",")))
-    cifar_data = csv_features[1:,1:-2] #with index and labels
-    cifar_data_norm = normalize(cifar_data)
-    return cifar_data_norm
-
-#Transform the cifar-data into WorkMATe memory size
-def cifar_to_memory(cifar_data):
-    nl = 30 #from workmate
-    W_Sl  = np.random.sample((nl,cifar_data.shape[1]))*(-1)+0.5 #from workmate
-    W_Sl, mask = drop_out(W_Sl, 0.1)
-    memory_set = [W_Sl.dot(item) for item in cifar_data]
-    scaler = MinMaxScaler() #scale between 0 and 1
-    cifar_data_mem = scaler.fit_transform(memory_set)
-    return cifar_data_mem
-
-def construct_random_input(trial_type, encoding, N, A):
-    #Create two random inputs: y (inp1) and z(inp2)
-    inp1, inp2 = np.random.sample((N,)), np.random.sample((N,))
-    #inp = np.random.choice([0, 1], size=(self.N,)) #The visual input is binary
-    #inp = (np.random.sample((self.N,)) - 0.5)*2 #range between -1 and 1
-    
-    if trial_type == 1: #match
-    #there are two options: inp1 and inp2 are exactly equal or inp2 is transformed by a random linear encoding
-        if encoding == 'identity':
-            inp2 = inp1 
-        if encoding == 'linear':
-            inp_transform = A.dot(inp1) #dot product with random matrix
-            inp2 = scale_linear_input(inp_transform, -1, 1) #normalized to fit between 1 and -1
-    return inp1, inp2
-
-def construct_cifar_input(trial_type, encoding, A, cifar_data):
-    #Create two random inputs: y (inp1) and z(inp2)
-    random_indices = np.random.choice(cifar_data.shape[0], size=2, replace=False)
-    inp1, inp2 = cifar_data[random_indices[0],:], cifar_data[random_indices[1],:]
-    
-    if trial_type == 1: #match
-    #there are two options: inp1 and inp2 are exactly equal or inp2 is transformed by a random linear encoding
-        if encoding == 'identity':
-            inp2 = inp1 
-        if encoding == 'linear':
-            inp_transform = A.dot(inp1) #dot product with random matrix
-            inp2 = scale_linear_input(inp_transform, -1, 1) #normalized to fit between 1 and -1
-    return inp1, inp2
-
-def construct_input(trial_type, data_type, encoding, N, A, cifar_data):
-    #Create two random inputs: y (inp1) and z(inp2)
-    if data_type == 'random':
-        inp1, inp2 = np.random.sample((N,)), np.random.sample((N,))
-        #inp = np.random.choice([0, 1], size=(self.N,)) #The visual input is binary
-        #inp = (np.random.sample((self.N,)) - 0.5)*2 #range between -1 and 1
-    elif data_type == 'cifar':
-        random_indices = np.random.choice(cifar_data.shape[0], size=2, replace=False)
-        inp1, inp2 = cifar_data[random_indices[0],:], cifar_data[random_indices[1],:]
-    
-    if trial_type == 1: #match
-    #there are two options: inp1 and inp2 are exactly equal or inp2 is transformed by a random linear encoding
-        if encoding == 'identity':
-            inp2 = inp1 
-        if encoding == 'linear':
-            inp_transform = A.dot(inp1) #dot product with random matrix
-            inp2 = scale_linear_input(inp_transform, -1, 1) #normalized to fit between 1 and -1
-    return inp1, inp2
-
-#cifar_data = load_cifar_data()
-#cifar_data = cifar_to_memory(cifar_data) #transform the dataset to memory inputs
-
-#%% Define the matchnet architecture and learning rules: one input in, x4 out
-
-class MatchNet(object):
-    def __init__(self, N=10, encoding='identity', data_type ='random', seed = 30):
-        super(MatchNet, self).__init__()
-        
+class MatchNet_Data(object):
+    def __init__(self, N=10, encoding='identity', data_type='random', seed = 30):
+        super(MatchNet_Data, self).__init__()
         np.random.seed(seed)
-        #Matchnet encoding type, data type and constants
+        
+        #Matchnet data type and encoding relationship between matching inputs
+        self.N = N
         self.encoding = encoding    #encoding can be identity (default) and linear
         self.data_type = data_type  #data can be random (default) and cifar
-        self.lr = 0.003
-        self.p_eq = 0.5             #probability of equal inputs
-        self.alpha_low = 2.7        #sigmoid slope for N < 400
-        self.alpha_high = 1.0       #sigmoid slope for N >= 400
-        
+        self.p_eq = 0.5             #probability of equal inputs (ratio trial types)
+    
+        if data_type == 'cifar':
+            with open("CIFAR_10_kaggle_feature_2.csv", 'r') as f:
+                csv_features = np.array(list(csv.reader(f, delimiter=",")))
+            cifar_data_not_norm = csv_features[1:,1:-2] #with index and labels
+            self.cifar_data = normalize(cifar_data_not_norm)
+            self.N = self.cifar_data.shape[0]
+    
         #Define matrix transformation for linear encoding
         if encoding == 'linear':
             self.A = np.random.uniform(-1, 1, (self.N, self.N))
         else: self.A = 0
+    
+    #Transform the cifar-data into WorkMATe memory size
+    def cifar_to_memory(cifar_data):
+        nl = 30 #from workmate
+        W_Sl  = np.random.sample((nl,cifar_data.shape[1]))*(-1)+0.5 #from workmate
+        W_Sl, mask = drop_out(W_Sl, 0.1)
+        memory_set = [W_Sl.dot(item) for item in cifar_data]
+        scaler = MinMaxScaler() #scale between 0 and 1
+        cifar_data_mem = scaler.fit_transform(memory_set)
+        return cifar_data_mem
+    
+    def sample_input(self):
+        #0 is no-match, 1 is match
+        trial_type = np.random.choice([0,1], p=[1-self.p_eq, self.p_eq]) #p_eq is match probability
+        
+        if self.data_type == 'random':
+            #Create two random inputs: y (inp1) and z(inp2)
+            inp1, inp2 = np.random.sample((N,)), np.random.sample((N,))
+            #inp = np.random.choice([0, 1], size=(self.N,)) #The visual input is binary
+            #inp = (np.random.sample((self.N,)) - 0.5)*2 #range between -1 and 1
+        elif self.data_type == 'cifar':
+            #Create two random inputs: y (inp1) and z(inp2)
+            random_indices = np.random.choice(self.N, size=2, replace=False)
+            inp1, inp2 = self.cifar_data[random_indices[0],:], self.cifar_data[random_indices[1],:]
+        
+        if trial_type == 1: #match
+        #there are two options: inp1 and inp2 are exactly equal or inp2 is transformed by a random linear encoding
+            if self.encoding == 'identity':
+                inp2 = inp1 
+            if self.encoding == 'linear':
+                inp_transform = self.A.dot(inp1) #dot product with random matrix
+                inp2 = scale_linear_input(inp_transform, -1, 1) #normalized to fit between 1 and -1
+        
+        #Calculate cosine similarity between incoming vectors for later evaluation
+        cosine_corr = cosine_similarity(inp1, inp2)
+        
+        return inp1, inp2, trial_type, cosine_corr    
+
+#%% Define the matchnet architecture and learning rules: one input in, x4 out
+
+class MatchNet(object):
+    def __init__(self, inp1, inp2, seed = 30):
+        super(MatchNet, self).__init__()
+        np.random.seed(seed)
+        
+        self.lr = 0.003
+        self.alpha_low = 2.7        #sigmoid slope for N < 400
+        self.alpha_high = 1.0       #sigmoid slope for N >= 400
         
         #Define input size N
-        if data_type == 'random':
-            self.N = N #length input vector, input1 and input2 have the same length
-        elif data_type == 'cifar':
-            #When CIFAR is chosen, N is overwritten
-            #cifar_data_mem = cifar_to_memory(cifar_data)
-            inp1, inp2 = construct_input(0, 'cifar', self.encoding, N, self.A, cifar_data)
-            self.N = len(inp1) #choose either cifar_data or cifar_data_mem
+        if len(inp1) != len(inp2):
+            raise Exception("The length of the two inputs are not the same")
+        self.N = len(inp1)
         
         #Define size next layers
         nx1 = 2*self.N
@@ -172,29 +150,16 @@ class MatchNet(object):
         self.W_x1x2, self.mask_W_x1x2, self.delta_W_x1x2, self.x2 = init_layer(nx1, nx2, 0.8)
         self.W_x2x3, self.mask_W_x2x3, self.delta_W_x2x3, self.x3 = init_layer(nx2, nx3, 0.3) 
         
-    def step(self):
-        x1, trial_type, cosine_corr = self.get_input()
-        x2, x3, x4 = self._feedforward(x1)
-        self._feedback(x1, x2, x3)
-        return x4, trial_type, cosine_corr
-        
-    def get_input(self):
-        #0 is no-match, 1 is match
-        trial_type = np.random.choice([0,1], p=[1-self.p_eq, self.p_eq]) #p_eq is match probability
-        
-        #Get new input
-        if self.data_type == 'random':
-            inp1, inp2 = construct_random_input(trial_type, self.encoding, self.N, self.A)
-        if self.data_type == 'cifar':
-            inp1, inp2 = construct_cifar_input(trial_type, self.encoding, self.A, cifar_data)
-        x1 = np.concatenate((inp1, inp2)) #The combined activity (length 2N)
-        
-        #Calculate cosine similarity between incoming vectors for later evaluation
-        cosine_corr = cosine_similarity(inp1, inp2)
-        
-        return x1, trial_type, cosine_corr
+    def step(self, inp1, inp2):
+        #The input is the concatenated activity (length 2N)
+        x1 = np.concatenate((inp1, inp2))
+        #Do a feedforward pass and compute the output
+        x2, x3, x4 = self.feedforward(x1)
+        #Locally learn the weights using the Anti-hebbian learning rule
+        self.feedback(x1, x2, x3)
+        return x4
     
-    def _feedforward(self, x1):
+    def feedforward(self, x1):
         #Transfer function is the hyperbolic tangent, information is passed forward by a dot product
         x2_in = self.W_x1x2.dot(x1)
         x2 = np.tanh(self.alpha_low*x2_in)
@@ -206,7 +171,7 @@ class MatchNet(object):
         #The fourth layer selects the most active third layer neuron
         return x2, x3, max(x3)
     
-    def _feedback(self, x1, x2, x3):
+    def feedback(self, x1, x2, x3):
         def learn_weights(W, mask, a1, a2):
             #Anti-Hebbian Learning rule
             delta_W = -self.lr*np.outer(a2, a1) #a learning factor for each combination of the pre and postsynaptic neurons
@@ -227,11 +192,12 @@ class MatchNet(object):
 
 #%% Function for the training of the Matchnet
 
-def train_matchnet(network, t_max=10000, t_eval=1000):
+def train_matchnet(matchnet, t_max=10000, t_eval=1000):
     eval_data = []
     for i in range(t_max):
         #there is no feedback, no sequentiality, each trial is one independent time step
-        x4, trial_type, cosine_corr = matchnet.step()
+        inp1, inp2, trial_type, cosine_corr = matchnet_data.sample_input() 
+        x4 = matchnet.step(inp1, inp2)
         
         #print trial_progress
         if i % 1000 == 0:
@@ -283,22 +249,31 @@ def plot_eval(x4, trial_type, cos_corr):
     plt.legend(bbox_to_anchor=(1.05, 1.0), loc='upper left')
     plt.show()       
 
-#%% Run the neural comparator
+#%% Run the neural comparator with random or sparse CIFAR-10 data
 
 if __name__ == '__main__':
     t_max = 10000          #number of training steps (10**7 in the paper)
     t_eval = 1000          #number of steps that are evaluated (10**6 in paper)
     N = 30                 #length of the input vector
 
+    #Initialize the data (data type and encoding)
+    matchnet_data = MatchNet_Data(N)
+    #Initialize the network using one sample of the data
+    inp1, inp2, trial_type, cosine_corr = matchnet_data.sample_input()
+    matchnet = MatchNet(inp1, inp2) 
+
     tic()
-    #load CIFAR-10 dataset when used
-    cifar_data = load_cifar_data()
-    #initialize the network
-    #matchnet = MatchNet(N, data_type = 'cifar')
-    matchnet = MatchNet(N)  
     #train the network in an unsupervised manner, print every dividable by 10000
     matchnet, eval_dt = train_matchnet(matchnet, t_max, t_eval) 
-    #plot the performance
-    plot_eval(eval_dt[0], eval_dt[1], eval_dt[2])
+    plot_eval(eval_dt[0], eval_dt[1], eval_dt[2]) #plot the performance
     toc()
+
+    #Run the trained neural comparator for new samples
+    #High values for non-match trials, low values for match trials
+    inp4, inp5 = np.random.sample((len(inp1),)), np.random.sample((len(inp1),))
+    inp6 = inp4
+        
+    x4 = matchnet.step(inp4, inp6)
+    print(x4)
+
 
